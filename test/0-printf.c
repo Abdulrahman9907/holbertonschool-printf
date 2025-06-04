c480680b9b8b175f1ded9a236432d78a1b57685c
root <ryan-055@outlook.com>
2025-06-04 18:16:00 +0000

    string and char implemenation

--- test/0-printf.c
+++ test/0-printf.c
@@ -18,35 +18,63 @@ int _print_string(const char *str)
   int count = 0;
 
   if (!str)
-    str = "NULL";
+  {
+	str = "NULL";
+  }
 
 while (*str)
-_putchar(*str);
-str++;
-count++;
+{
+	_putchar(*str);
+	str++;
+	count++;
+}
 
-return count;
+return (count);
 }
 
 int _printf(const char *format, ...)
 {
-int num = 0;
+int num = 0, ch;
 const char *str;
-va_list argc;
+va_list args;
 
-va_start(argc, format);
-
-if (*format == '%')
-format++;
+va_start(args, format);
 
 while(*format)
 {
-if (*format == 's')
-str = va_arg(argc, const char*);
-num += _print_string(str);
-break;
+        if (*format == '%' && *(format + 1))
+        {
+		format++;
+        switch(*format)
+        {
+                case 's': {
+
+                        str = va_arg(args, const char*);
+                        num += _print_string(str);
+                        break;
+                          }
+		case 'c': {
+
+                        ch = va_arg(args, int);
+                        num += _putchar(ch);
+                        break;
+                          }
+		default: {
+			_putchar('%');
+			_putchar(*format);
+			num += 2;
+			break;
+		 	}
+	}
+	}
+	else
+	{
+		_putchar(*format);
+		num++;
+	}
+        format++;
 }
 
-
+va_end(args);
 return(num);
 }
---
+++ test/main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+
+int main(void)
+{
+
+	_printf("String:[%s]\n", "I am a string !");
+	printf("String:[%s]\n", "I am a string !");
+	_printf("Character:[%c]\n", 'H');
+    printf("Character:[%c]\n", 'H');
+	return (0);
+}
